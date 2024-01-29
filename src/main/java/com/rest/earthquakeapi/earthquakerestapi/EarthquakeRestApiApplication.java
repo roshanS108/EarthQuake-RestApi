f77b5c46ec00c39e0ba1d4fcde614e56e6e4a417
= <roshanshoti100@gmail.com>
2024-01-29 11:08:02 -0500

    first commit

---
+++ src/main/java/com/rest/earthquakeapi/earthquakerestapi/EarthquakeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.rest.earthquakeapi.earthquakerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthquakeRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/earthquakeapi/earthquakerestapi/EarthquakeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.earthquakeapi.earthquakerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthquakeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
