b3b9eb5352161030414ed0ff56bbd1558c7ce7e8
= <roshanshoti100@gmail.com>
2024-02-06 11:20:13 -0500

    tested! works successfully

--- src/main/java/com/rest/earthquakeapi/XMLParsing/IdParser.java
+++ src/main/java/com/rest/earthquakeapi/XMLParsing/IdParser.java
@@ -1,4 +1,26 @@
 package com.rest.earthquakeapi.XMLParsing;
+import java.util.*;
+import com.rest.earthquakeapi.model.QuakeEntry;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
-public class IdParser {
+public class IdParser implements ElementParser<String>{
+
+    @Override
+    public String parseElement(Element element) {
+        NodeList idNode = element.getElementsByTagName("id");
+
+        if(idNode!=null && idNode.getLength()>0){
+            String s2 = idNode.item(0).getChildNodes().item(0).getNodeValue();
+            String id = s2.substring(s2.indexOf(':', s2.indexOf(':', s2.indexOf(':') + 1) + 1) + 1);
+
+            return id;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean satisfies() {
+        return false;
+    }
 }
