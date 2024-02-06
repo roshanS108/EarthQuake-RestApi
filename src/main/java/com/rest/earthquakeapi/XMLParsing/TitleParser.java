3f59461ba4578bd4fe2fadbf37fad912bf5b9dde
= <roshanshoti100@gmail.com>
2024-02-06 11:22:18 -0500

    title parser works successfully

--- src/main/java/com/rest/earthquakeapi/XMLParsing/TitleParser.java
+++ src/main/java/com/rest/earthquakeapi/XMLParsing/TitleParser.java
@@ -4,38 +4,28 @@ import com.rest.earthquakeapi.model.QuakeEntry;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-public class TitleMagParser implements ElementParser{
+public class TitleParser implements ElementParser {
 
-    public TitleMagParser(){
+    public TitleParser() {
 
     }
+
     @Override
-    public void parseElement(Element element, QuakeEntry quakeEntry) {
+    public String parseElement(Element element) {
 
         //retrieves the "title" info of the place where earth quake occurred. ex --->75km WSW of Cantwell, Alaska
         NodeList t2 = element.getElementsByTagName("title");
 
-        if (t2 != null && t2.getLength()>0){
-            String s2 = t2.item(0).getChildNodes().item(0).getNodeValue();
-            String mags = s2.substring(2,s2.indexOf(" ",2));
-            System.out.println("the mags is: " + mags);
-            double mag = 0.0;
-            if (mags.contains("?")) {
-                mag = 0.0;
-                System.err.println("unknown magnitude in data");
-            }
-            else {
-                mag = Double.parseDouble(mags);
-                //System.out.println("mag= "+mag);
-            }
-            int sp = s2.indexOf(" ",5); //6
-            String title = s2.substring(sp + 1);
-            System.out.println("the title is: " + title);
-            if (title.startsWith("-")){
-                int pos = title.indexOf(" ");
-                title = title.substring(pos+1);
-            }
+        String s2 = t2.item(0).getChildNodes().item(0).getNodeValue();
+
+        int sp = s2.indexOf(" ", 5); //6
+        String title = s2.substring(sp + 1);
+        System.out.println("the title is: " + title);
+        if (title.startsWith("-")) {
+            int pos = title.indexOf(" ");
+            title = title.substring(pos + 1);
         }
+        return title;
     }
 
     @Override
@@ -43,8 +33,9 @@ public class TitleMagParser implements ElementParser{
         return false;
     }
 
-    @Override
-    public Object getParsedData() {
-        return new TitleMagParser(la)
-    }
+
 }
+
+
+
+
