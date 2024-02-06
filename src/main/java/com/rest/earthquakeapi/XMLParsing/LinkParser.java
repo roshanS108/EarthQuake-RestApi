910ca830dc71860747b9e7984a9567c0a4b050a9
= <roshanshoti100@gmail.com>
2024-02-06 11:21:03 -0500

    tested! works successfully, linkparser class

--- src/main/java/com/rest/earthquakeapi/XMLParsing/LinkParser.java
+++ src/main/java/com/rest/earthquakeapi/XMLParsing/LinkParser.java
@@ -1,4 +1,31 @@
 package com.rest.earthquakeapi.XMLParsing;
 
-public class LinkParser {
+import com.rest.earthquakeapi.model.QuakeEntry;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class LinkParser implements ElementParser<String>{
+    @Override
+    public String parseElement(Element element) {
+
+        String link = "";
+        //retrieves the <link> element
+        NodeList linkList = element.getElementsByTagName("link");
+
+        if (linkList != null && linkList.getLength() > 0) {
+            Node linkNode = linkList.item(0); // Get the first <link> element
+
+            String hrefValue = ((Element) linkNode).getAttribute("href");
+            link = hrefValue;
+            System.out.println("The link is: " + link);
+        }
+        return link;
+
+    }
+
+    @Override
+    public boolean satisfies() {
+        return false;
+    }
 }
