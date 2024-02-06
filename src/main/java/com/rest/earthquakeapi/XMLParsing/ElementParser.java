415f47c8a79bfe94344f0a906ad4ff8c6456d741
= <roshanshoti100@gmail.com>
2024-02-06 11:22:52 -0500

    updating elementparser interface to accept generic

--- src/main/java/com/rest/earthquakeapi/XMLParsing/ElementParser.java
+++ src/main/java/com/rest/earthquakeapi/XMLParsing/ElementParser.java
@@ -1,4 +1,20 @@
 package com.rest.earthquakeapi.XMLParsing;
 
-public interface ElementParser {
+import com.rest.earthquakeapi.model.QuakeEntry;
+import org.w3c.dom.Element;
+
+public interface ElementParser<T> {
+
+
+    T parseElement(Element element);
+
+
+
+
+
+
+
+    boolean satisfies();
+
+
 }
