6268b4853764640aba97cc7ac3bcd7efcc8c403b
= <roshanshoti100@gmail.com>
2024-02-08 19:21:59 -0500

    adding earthquakesnear method for implementation

--- src/main/java/com/rest/earthquakeapi/service/EarthquakeDataProcessor.java
+++ src/main/java/com/rest/earthquakeapi/service/EarthquakeDataProcessor.java
@@ -1,5 +1,6 @@
 package com.rest.earthquakeapi.service;
 
+import com.rest.earthquakeapi.apache.Location;
 import com.rest.earthquakeapi.model.QuakeEntry;
 
 import java.util.ArrayList;
@@ -14,6 +15,11 @@ public interface EarthquakeDataProcessor {
     ArrayList<QuakeEntry> filterByMagnitude(ArrayList<QuakeEntry> quakeData, double minMag);
 
     List<QuakeEntry> bigQuakes();
+
+
+    //gets all the earthquake that are less than @disMax from Location @from
+    List<QuakeEntry> earthQuakesNearMe(double distMax, Location from);
+
 
 
 }
