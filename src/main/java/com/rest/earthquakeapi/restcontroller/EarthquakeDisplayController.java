aa5f5b8ecbc40fc0a258db93ecf93c5539986fdc
= <roshanshoti100@gmail.com>
2024-02-08 19:23:05 -0500

    adding /nearby endpoints, for retrieivng earthquakes that are less than some distance

--- src/main/java/com/rest/earthquakeapi/restcontroller/EarthquakeDisplayController.java
+++ src/main/java/com/rest/earthquakeapi/restcontroller/EarthquakeDisplayController.java
@@ -1,15 +1,16 @@
 package com.rest.earthquakeapi.restcontroller;
+import com.rest.earthquakeapi.apache.Location;
 import com.rest.earthquakeapi.model.QuakeEntry;
 import com.rest.earthquakeapi.service.EarthquakeDataProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/earthquakes")
 public class EarthquakeDisplayController {
 
     private EarthquakeDataProcessor earthquakeDataProcessor;
@@ -18,8 +19,7 @@ public class EarthquakeDisplayController {
     public EarthquakeDisplayController(EarthquakeDataProcessor earthquakeDataProcessor){
         this.earthquakeDataProcessor = earthquakeDataProcessor;
     }
-
-    @GetMapping("/earthquakes")
+    @GetMapping("/bigQuakes")
     public ResponseEntity<List<QuakeEntry>> getBigQuakes() {
         try {
             List<QuakeEntry> largeQuakes = earthquakeDataProcessor.bigQuakes();
@@ -29,6 +29,27 @@ public class EarthquakeDisplayController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+    @GetMapping("/nearby")
+    public ResponseEntity<List<QuakeEntry>> getNearByEarthQuakes(
+                        @RequestParam double distMax,
+                        @RequestParam double latitude,
+                        @RequestParam double longitude){
+        System.out.println("dist max is: " + distMax);
+        Location location = new Location(latitude, longitude);
+        System.out.println("location is: " + location);
+        try {
+            List<QuakeEntry> nearEarthQuakes = earthquakeDataProcessor.earthQuakesNearMe(distMax, location);
+            return ResponseEntity.ok(nearEarthQuakes);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+
+
+
+
 
 
 
