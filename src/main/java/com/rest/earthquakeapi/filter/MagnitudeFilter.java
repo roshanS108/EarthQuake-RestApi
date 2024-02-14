039147e0dd914d3e8737983d026827590b9080dc
= <roshanshoti100@gmail.com>
2024-02-14 11:38:38 -0500

    implemented basic requirements for satisfies method in magnitudefilter class

--- src/main/java/com/rest/earthquakeapi/filter/MagnitudeFilter.java
+++ src/main/java/com/rest/earthquakeapi/filter/MagnitudeFilter.java
@@ -1,4 +1,21 @@
 package com.rest.earthquakeapi.filter;
 
-public class MagnitudeFilter {
+import com.rest.earthquakeapi.model.QuakeEntry;
+
+public class MagnitudeFilter implements Filter{
+
+    private double minMag;
+    private double maxMag;
+
+    public MagnitudeFilter(double minMag, double maxMag){
+        this.minMag = minMag;
+        this.maxMag = maxMag;
+
+    }
+    @Override
+    public boolean satisfies(QuakeEntry qe) {
+        return qe.getMagnitude() >= minMag && qe.getMagnitude() <= maxMag;
+    }
+
+
 }
