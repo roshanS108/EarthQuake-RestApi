c07880d49dda317c44ae4d7d83d906e46dbecba2
= <roshanshoti100@gmail.com>
2024-02-14 11:34:20 -0500

    implemented basic requirements for satisfies method in distancefilter class

--- src/main/java/com/rest/earthquakeapi/filter/DistanceFilter.java
+++ src/main/java/com/rest/earthquakeapi/filter/DistanceFilter.java
@@ -1,4 +1,19 @@
 package com.rest.earthquakeapi.filter;
 
-public class DistanceFilter {
+import com.rest.earthquakeapi.apache.Location;
+import com.rest.earthquakeapi.model.QuakeEntry;
+
+public class DistanceFilter implements Filter{
+
+    private Location location;
+    private double maxDistance;
+
+    public DistanceFilter(Location location, double maxDistance){
+        this.location = location;
+        this.maxDistance = maxDistance;
+    }
+    @Override
+    public boolean satisfies(QuakeEntry qe) {
+        return qe.getLocation().distanceTo(location) < maxDistance;
+    }
 }
