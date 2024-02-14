9dfba4908e9e30bcbcf153005097fd30e85c60ba
= <roshanshoti100@gmail.com>
2024-02-14 11:32:55 -0500

    implemented basic requirements for satisfies method

--- src/main/java/com/rest/earthquakeapi/filter/DepthFilter.java
+++ src/main/java/com/rest/earthquakeapi/filter/DepthFilter.java
@@ -1,4 +1,19 @@
 package com.rest.earthquakeapi.filter;
 
-public class DepthFilter {
+import com.rest.earthquakeapi.apache.Location;
+import com.rest.earthquakeapi.model.QuakeEntry;
+
+public class DepthFilter implements Filter{
+
+    private double minDepth;
+    private double maxDepth;
+
+    public DepthFilter(double minDepth, double maxDepth){
+        this.minDepth = minDepth;
+        this.maxDepth = maxDepth;
+    }
+    @Override
+    public boolean satisfies(QuakeEntry qe) {
+        return qe.getDepth()>=maxDepth && qe.getDepth()<=maxDepth;
+    }
 }
