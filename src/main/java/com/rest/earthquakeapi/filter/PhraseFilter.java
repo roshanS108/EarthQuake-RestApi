45fb4f9f5cc830ec4cdca653296cc824f287450b
= <roshanshoti100@gmail.com>
2024-02-14 11:41:33 -0500

    finished with basic requirement for phrasefilter class

--- src/main/java/com/rest/earthquakeapi/filter/PhraseFilter.java
+++ src/main/java/com/rest/earthquakeapi/filter/PhraseFilter.java
@@ -1,4 +1,25 @@
 package com.rest.earthquakeapi.filter;
 
-public class PhraseFilter {
+import com.rest.earthquakeapi.model.QuakeEntry;
+
+public class PhraseFilter implements Filter{
+
+    private String typeOfRequest;
+    private String phraseToSearch;
+
+    public PhraseFilter(String typeOfRequest, String phraseToSearch) {
+        this.typeOfRequest = typeOfRequest;
+        this.phraseToSearch = phraseToSearch;
+    }
+
+    @Override
+    public boolean satisfies(QuakeEntry qe) {
+        String title = qe.getInfo();
+        return switch (typeOfRequest) {
+            case "start" -> title.startsWith(phraseToSearch);
+            case "end" -> title.endsWith(phraseToSearch);
+            case "any" -> title.contains(phraseToSearch);
+            default -> false;
+        };
+    }
 }
