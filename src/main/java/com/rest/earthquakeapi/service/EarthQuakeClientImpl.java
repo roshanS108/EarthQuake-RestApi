c1d1229ce785a73fba987c9c9b475cc61c158d98
= <roshanshoti100@gmail.com>
2024-03-04 06:58:31 -0500

    adding java doc for earthQuakesNearMe method

--- src/main/java/com/rest/earthquakeapi/service/EarthQuakeClientImpl.java
+++ src/main/java/com/rest/earthquakeapi/service/EarthQuakeClientImpl.java
@@ -84,6 +84,28 @@ public class EarthQuakeClientImpl implements EarthquakeDataProcessor {
                     qe.getInfo());
         }
     }
+    /**
+     * Finds and returns a list of earthquakes that occurred near a specified location.
+     * How it works:
+     * 1. It creates an instance of EarthQuakeParser to read earthquake data.
+     * 2. It specifies the source of the earthquake data. use a URL to fetch live data instead.)
+     * 3. It reads the earthquake data into a list of QuakeEntry objects.
+     * 4. It prints out the number of earthquakes read from the data source.
+     * 5. It filters the list of QuakeEntry objects to find those earthquakes that are within
+     *    a specified maximum distance ('distMax') from a 'from' location.
+     * 6. For each earthquake found within the specified distance, it prints out the distance
+     *    to the earthquake from a hard-coded location (in this case, Bridgeport, CA) and the
+     *    earthquake's information, --->soon making the location dynamic.
+     * 7. It prints out the total number of earthquakes found that match the criteria.
+     * 8. Finally, it returns the filtered list of close earthquakes.
+     * Parameters:
+     *  - distMax: The maximum distance (in kilometers) from the 'from' location to consider
+     *             an earthquake as being "near".
+     *  - from: The location from which the distance to each earthquake is measured.
+     * Returns:
+     *  - A list of QuakeEntry objects, each representing an earthquake that occurred within
+     *    the specified maximum distance from the 'from' location.
+     */
     @Override
     public List<QuakeEntry> earthQuakesNearMe(double distMax, Location from) {
         EarthQuakeParser parser = new EarthQuakeParser();
