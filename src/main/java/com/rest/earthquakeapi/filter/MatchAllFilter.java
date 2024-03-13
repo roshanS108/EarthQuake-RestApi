7160684b3983b1b480dc9187f9eb3db3c2a31958
= <roshanshoti100@gmail.com>
2024-03-13 13:49:04 -0400

    class for matching all filters

--- src/main/java/com/rest/earthquakeapi/filter/MatchAllFilter.java
+++ src/main/java/com/rest/earthquakeapi/filter/MatchAllFilter.java
@@ -1,4 +1,29 @@
 package com.rest.earthquakeapi.filter;
 
-public class MatchAllFilter {
+import com.rest.earthquakeapi.model.QuakeEntry;
+
+import java.util.ArrayList;
+
+public class MatchAllFilter implements Filter{
+
+    private ArrayList<Filter> filters;
+
+    public MatchAllFilter(){
+        filters = new ArrayList<>();
+    }
+
+    public void addFilter(Filter f){
+        filters.add(f);
+    }
+
+    @Override
+    public boolean satisfies(QuakeEntry qe) {
+        for(Filter filter: filters){
+            if(!filter.satisfies(qe)){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
