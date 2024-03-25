3a671611dcd566fc0f4e3a9e4198dcd2fe6ee566
= <roshanshoti100@gmail.com>
2024-03-25 14:23:22 -0400

    this class handles data that are not found, extends RuntTimeException to customize the error

--- src/main/java/com/rest/earthquakeapi/exception/QuakeDataNotFoundException.java
+++ src/main/java/com/rest/earthquakeapi/exception/QuakeDataNotFoundException.java
@@ -1,5 +1,16 @@
 package com.rest.earthquakeapi.exception;
 
-public class QuakeDataNotFoundException
-{
+public class QuakeDataNotFoundException extends RuntimeException{
+    public QuakeDataNotFoundException(String message) {
+        super(message);
+    }
+
+    public QuakeDataNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public QuakeDataNotFoundException(Throwable cause) {
+        super(cause);
+    }
 }
+
