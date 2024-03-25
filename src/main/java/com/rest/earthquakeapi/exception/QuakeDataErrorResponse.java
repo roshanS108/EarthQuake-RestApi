b3c7ea6593875527d279cdb0f364db4a87ad9f23
= <roshanshoti100@gmail.com>
2024-03-25 14:09:20 -0400

    a class to handle earthquake data that is not found

--- src/main/java/com/rest/earthquakeapi/exception/QuakeDataErrorResponse.java
+++ src/main/java/com/rest/earthquakeapi/exception/QuakeDataErrorResponse.java
@@ -13,28 +13,22 @@ public class QuakeDataErrorResponse extends RuntimeException{
         this.message = message;
         this.timeStamp = timeStamp;
     }
-
     public int getStatus() {
         return status;
     }
-
     public void setStatus(int status) {
         this.status = status;
     }
-
     @Override
     public String getMessage() {
         return message;
     }
-
     public void setMessage(String message) {
         this.message = message;
     }
-
     public String getTimeStamp() {
         return timeStamp;
     }
-
     public void setTimeStamp(String timeStamp) {
         this.timeStamp = timeStamp;
     }
