591062811982b8b0265cf685f07d8a7894774d7c
= <roshanshoti100@gmail.com>
2024-04-16 12:13:55 -0400

    config class designed to create custom bean of class EarthquakeParser and providing to all the beans that rely on it

--- src/main/java/com/rest/earthquakeapi/config/EarthQuakeParserConfig.java
+++ src/main/java/com/rest/earthquakeapi/config/EarthQuakeParserConfig.java
@@ -1,4 +1,16 @@
 package com.rest.earthquakeapi.config;
 
+import com.rest.earthquakeapi.ParserManager.EarthQuakeParser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class EarthQuakeParserConfig {
+
+    @Bean
+    public EarthQuakeParser earthQuakeParser(){
+        return new EarthQuakeParser();
+    }
+
+
 }
