b19f0c2a1e275128ef6676c83aa717f2127e5f50
= <roshanshoti100@gmail.com>
2024-04-19 13:37:22 -0400

    added jacksonconfig class to parse java objec to json

--- src/main/java/com/rest/earthquakeapi/config/JacksonConfig.java
+++ src/main/java/com/rest/earthquakeapi/config/JacksonConfig.java
@@ -1,2 +1,18 @@
-package com.rest.earthquakeapi.config;public class JacksonConfig {
+package com.rest.earthquakeapi.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return objectMapper;
+    }
+
 }
