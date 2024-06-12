0a8c45ab3e43aa7d4873a0ed9b33b14966acb6de
= <roshanshoti100@gmail.com>
2024-06-12 19:41:50 -0400

    adding differntsort class to use in controller class

--- src/main/java/com/rest/earthquakeapi/sorting/DifferentSorters.java
+++ src/main/java/com/rest/earthquakeapi/sorting/DifferentSorters.java
@@ -3,44 +3,43 @@ package com.rest.earthquakeapi.sorting;
 import com.rest.earthquakeapi.ParserManager.EarthQuakeParser;
 import com.rest.earthquakeapi.apache.Location;
 import com.rest.earthquakeapi.model.QuakeEntry;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.ArrayList;
 import java.util.Collections;
-
 public class DifferentSorters {
     private final EarthQuakeParser parser;
-
+    @Value("${earthquake.source}")
+    private String source;
     public DifferentSorters() {
         this.parser = new EarthQuakeParser();
     }
 
-    public void sortByDistance() {
-        EarthQuakeParser parser = new EarthQuakeParser();
-        String source = "data/nov20quakedata.atom";
-        ArrayList<QuakeEntry> list = parser.read(source);
-        Location where = new Location(35.9886, -78.9072);
-        Collections.sort(list, new DistanceComparator(where));
-        for (QuakeEntry qe : list) {
-            System.out.println(qe);
-        }
-    }
-
     public void sortByMagnitude() {
-        String source = "data/nov20quakedata.atom";
         ArrayList<QuakeEntry> list = parser.read(source);
         Collections.sort(list, new MagnitudeComparator());
         for (QuakeEntry qe : list) {
             System.out.println(qe);
         }
     }
-
     public void sortByTitle() {
-        EarthQuakeParser parser = new EarthQuakeParser();
-        String source = "data/nov20quakedata.atom";
         ArrayList<QuakeEntry> list = parser.read(source);
         Collections.sort(list, new TitleComparator());
         for (QuakeEntry qe : list) {
             System.out.println(qe);
         }
     }
+    public void sortByDepth() {
+        ArrayList<QuakeEntry> list = parser.read(source);
+        Collections.sort(list, new DepthComparator());
+        for (QuakeEntry qe : list) {
+            System.out.println(qe);
+        }
+    }
+    public void sortAll() {
+        sortByMagnitude();
+        sortByTitle();
+        sortByDepth();
+    }
+
 }
