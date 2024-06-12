11bdb5197f108ff6d62db3cfc95c0ca443f1dac3
= <roshanshoti100@gmail.com>
2024-06-12 19:42:14 -0400

    sorting by title using comparator

--- src/main/java/com/rest/earthquakeapi/sorting/TitleComparator.java
+++ src/main/java/com/rest/earthquakeapi/sorting/TitleComparator.java
@@ -1,2 +1,8 @@
-package com.rest.earthquakeapi.sorting;public class TitleComparator {
+package com.rest.earthquakeapi.sorting;
+import com.rest.earthquakeapi.model.QuakeEntry;
+import java.util.Comparator;
+public class TitleComparator implements Comparator<QuakeEntry> {
+    public int compare(QuakeEntry q1, QuakeEntry q2) {
+        return q1.getTitle().compareTo(q2.getTitle());
+    }
 }
